53377af491b86e02b306f9b905ca2a6564461167
alxsmora1 <alexis.mora1v@gmail.com>
2021-03-28 23:07:47 -0600

    import NavbarSearch component
    Use NavbarSearch child component to trigger the function that allows
    ContentBox to search a specific pokemon.
    Use render conditionals for display the data default or the data of the
    search.
    Add the state pokemon to store the data of an specific pokemon.
    Add the function handleSubmit to be triggered by the child component
    props.

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -3,6 +3,7 @@ import {
     Container,
     Row,
     Col,
+    Spinner,
     Card, 
     CardHeader, 
     CardImg, 
@@ -11,8 +12,9 @@ import {
     CardTitle
 } from 'reactstrap';
 import axios from 'axios';
+import NavbarSearch from './NavbarSearch';
 
-function ContentBox() {
+function ContentBox(props) {
     const [data, setData] = useState(null);
     useEffect(() => {
             let promises = [];
@@ -23,20 +25,48 @@ function ContentBox() {
 
             Promise.all(promises).then(function (responses) {
                 setData(responses);
-                console.log(responses);
             });
     }, []);
 
-    if (!data) {
-        return <h1>Loading...</h1>;
+    const [pokemon, setPokemon] = useState(null);
+    const handleSubmit = (spokemon) => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/' + spokemon).then(function (response) {
+            setPokemon(response.data);
+        });
     }
     
     return (
+        <>
+        <NavbarSearch onHandleSubmit={handleSubmit} />
+        {pokemon &&    
+        <Container>
+            <Row className="mt-3">
+            <Col xs="12" sm="6" md="3 mb-2" key={pokemon.id}>
+                <Card className="rounded shadow">
+                <CardHeader>
+                    <CardImg top width="100%" src={pokemon.sprites.front_default} />
+                </CardHeader>
+                <CardBody>
+                    <CardTitle className="text-center text-capitalize" tag="h5"> {pokemon.name}</CardTitle>
+                    <CardText>
+                        <strong>ID Pokedex:</strong> {pokemon.id} <br />
+                        <strong>Type:</strong> {pokemon.types[0].type.name} <br />
+                        <strong>Weight:</strong> {pokemon.weight/10}Kg <br /> 
+                        <strong>Height:</strong> {pokemon.height/10}M
+                    </CardText>
+                </CardBody>      
+                </Card>
+            </Col>
+            </Row>
+            </Container>
+        }
+        
+        {data && !pokemon &&
         <Container>
         <Row className="mt-3">
         {data.map((data) => 
-            <Col xs="12" sm="6" md="3 mb-2">
-                <Card className="rounded shadow" key={data.data.id}>
+            <Col xs="12" sm="6" md="3 mb-2" key={data.data.id}>
+                <Card className="rounded shadow">
                 <CardHeader>
                     <CardImg top width="100%" src={data.data.sprites.front_default} />
                 </CardHeader>
@@ -54,6 +84,15 @@ function ContentBox() {
         )}
         </Row>
         </Container>
+        }
+
+        {!data &&
+            <div className="text-center mt-5">
+            <Spinner color="primary" />
+            <h1>Loading data</h1>
+            </div>
+        }
+        </>
     );
 }
 
